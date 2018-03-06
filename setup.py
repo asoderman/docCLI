0f7e5365aa974121c5523f3a983fd3da637a8415
Alex Soderman <5639572+asoderman@users.noreply.github.com>
2018-03-05 21:27:44 -0500

    Version bump and include data

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ setup(
 	author_email='asoderman.as@gmail.com',
 	url='https://github.com/asoderman/docCLI',
 	description='A command line interface for Google Docs suite',
-	version='0.0.3',
+	version='0.0.4',
 	install_requires=[
 		'Click',
 		'google-api-python-client'
 		],
 	packages=find_packages(),
+	package_data={'': ['docCLI/credentials/client_secret.json']},
+	include_package_data=True,
 	entry_points='''
 	[console_scripts]
 	docs=docCLI.docs:open_docs
